92da5a4c9ccf4c29ed83f7963d6c8a68573ea369
AdaDataSci <adannad02@yahoo.com>
2017-05-11 19:36:41 -0400

    Add files via upload

---
+++ server.R
@@ -0,0 +1,33 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+
+library(shiny)
+library(caret)
+library(rpart)
+library(e1071)
+data(iris)
+
+
+set.seed(123)
+inTrain <- createDataPartition(y=iris$Species,p=0.7, list=FALSE)
+training <- iris[inTrain,]
+rpart <-train(factor(Species) ~ .,method="rpart",data=training)
+
+shinyServer(function(input, output) {  
+  Pred<-reactive({
+    Sepal.Length <- input$Sepal_L
+    Sepal.Width <- input$Sepal_W
+    Petal.Length <- input$Petal_L
+    Petal.Width <- input$Petal_W
+    predict(rpart,newdata = data.frame(Sepal.Length, 
+                                       Sepal.Width,Petal.Length,Petal.Width))
+  })
+  output$Pred <- renderPrint({Pred() })
+})
